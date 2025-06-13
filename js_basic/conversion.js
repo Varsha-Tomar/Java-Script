7b709eaab8c442e355110e5a510bda466014b981
Varsha <varsha.v.tomar@gmail.com>
2025-06-13 08:11:40 +0000

    conversion

---
+++ js_basic/conversion.js
@@ -0,0 +1,16 @@
+let score = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+
+// "33abc" => NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
