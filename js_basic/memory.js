37e6a4caefe79884ec3371c6650ac331d0fa6e63
Varsha <varsha.v.tomar@gmail.com>
2025-06-14 04:12:12 +0000

    memory

---
+++ js_basic/memory.js
@@ -0,0 +1,11 @@
+// Stact (primitive), Heap (non-primitive)
+
+let user = {
+    email: "user@google.com",
+    upi : "user@ybl"
+}
+
+let serTwo = user
+// userTwo is a reference to the same object in memory as
+
+console.log(user.email)
