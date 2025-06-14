eaec4edebc9dea0c771b5ce4bed78673232d79cd
Varsha <varsha.v.tomar@gmail.com>
2025-06-14 04:57:28 +0000

    edit

--- js_basic/memory.js
+++ js_basic/memory.js
@@ -9,4 +9,12 @@ let user = {
 let serTwo = user
 // userTwo is a reference to the same object in memory as
 
-console.log(user.email)
+console.log(user.email) 
+console.log(userTwo.email)
+// Changing userTwo will also change user
+userTwo.email = " "// userTwo is a reference to the same object in memory as user
+console.log(user.email) // userTwo is a reference to the same object in memory as user
+console.log(userTwo.email) // userTwo is a reference to the same object in memory as user
+// userTwo is a reference to the same object in memory as user
+// userTwo is a reference to the same object in memory as user
+// userTwo is a reference to the same object in memory as user
