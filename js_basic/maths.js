3db38afb6672ad603ff58db2d2d7c4951ac90e17
Varsha <varsha.v.tomar@gmail.com>
2025-06-16 04:19:36 +0000

    update math 1

--- js_basic/maths.js
+++ js_basic/maths.js
@@ -4,3 +4,10 @@ const balance = new Number(500)
 
 console.log(score)
 console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 34.5464
+
+console.log(otherNumber.toPrecision(3));  
