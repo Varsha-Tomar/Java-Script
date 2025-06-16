e0915379503805b59b0f30dfd18c95475273cbf1
Varsha <varsha.v.tomar@gmail.com>
2025-06-16 12:17:33 +0000

    nexted array

--- js_basic/2js_basic/array2.js
+++ js_basic/2js_basic/array2.js
@@ -5,5 +5,16 @@ marvel_heros.push(dc)
 
 console.log(marvel_heros);
 
-marvel_heros.concat(dc)
-console.log(marvel_heros)
+const a = marvel_heros.concat(dc)
+console.log(a)
+
+// spread operator ...
+
+const all_new_heros = [...marvel_heros,...dc]
+console.log(all_new_heros)
+
+const another_array = [2,5,6,[5,6,6],3,[1,2,4]];
+
+const real_another_array = another_array.flat(Infinity)
+
+console.log(real_another_array)
