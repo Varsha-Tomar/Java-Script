594542195ef14734fb02b8fe11fb5b0cd1d42039
Varsha <varsha.v.tomar@gmail.com>
2025-06-16 04:45:40 +0000

    random

--- js_basic/maths.js
+++ js_basic/maths.js
@@ -1,4 +1,18 @@
 // ************ maths functions **************
 
-console.log(Math);
-console.log(Math.abs(-7));
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(5.3));
+// console.log(Math.ceil(5.3));
+// console.log(Math.floor(5.3));
+// console.log(Math.min(2, 3, 4, 5, 6));   // array ki smallest value
+// console.log(Math.max(2, 3, 4, 5, 6));   // array ki largest value
+// console.log(Math.random());  // 0 se 1 ke beech random number
+
+// console.log((Math.random()*10)+1);  // 1 se 10 ke beech random number
+
+const min =10
+const max = 20
+
+Math.random()
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // 10 se 20 ke beech random number
