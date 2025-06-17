5b8fa06bdabd1cd19fb4add6280accfa0dbbef23
Varsha <varsha.v.tomar@gmail.com>
2025-06-17 05:53:26 +0000

    to concat the objects

--- js_basic/objectsPartTwo.js
+++ js_basic/objectsPartTwo.js
@@ -19,4 +19,12 @@ const regularUser ={
     }
 }
 
-console.log(regularUser.fullname.userfullname.firstname)
+// console.log(regularUser.fullname.userfullname.firstname)
+
+
+const obj1 ={1: "a", 2:"b"}
+const obj2 ={3: "a", 4: "b"}
+
+// const obj3 ={obj1, obj2}
+const obj3 = Object.assign(obj1,obj2)
+console.log(obj3)
