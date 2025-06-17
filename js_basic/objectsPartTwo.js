4d4c7e3b2bc945676feccb906f7910a4c8e2483f
Varsha <varsha.v.tomar@gmail.com>
2025-06-17 06:04:19 +0000

    update object

--- js_basic/objectsPartTwo.js
+++ js_basic/objectsPartTwo.js
@@ -26,5 +26,13 @@ const obj1 ={1: "a", 2:"b"}
 const obj2 ={3: "a", 4: "b"}
 
 // const obj3 ={obj1, obj2}
-const obj3 = Object.assign(obj1,obj2)
-console.log(obj3)
+// const obj3 = Object.assign({},obj1,obj2) // {}it is empty target object and it return the modified target object
+
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3)
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
