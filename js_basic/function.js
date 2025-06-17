d29748db008c85236f5f4274c588284ed7d69994
Varsha <varsha.v.tomar@gmail.com>
2025-06-17 07:51:16 +0000

    function udpdate

--- js_basic/function.js
+++ js_basic/function.js
@@ -1,6 +1,10 @@
 
 function addTwoNumbers(num1,num2) {
-    console.log(num1+num2)
+    let result = num1+num2;
+    return result   
+    // result ke baad kuch bhi print nhi hota hai
+
 }
 
+console.log(addTwoNumbers(6,8))
 addTwoNumbers(89,65)
