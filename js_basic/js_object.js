d55efacd9e48cca1c8398253dc5cde75ef240354
Varsha <varsha.v.tomar@gmail.com>
2025-06-17 05:23:57 +0000

    function object

--- js_basic/js_object.js
+++ js_basic/js_object.js
@@ -4,8 +4,13 @@
 
 // Object.create
 
+const mySym = Symbol("key1")
+
 const jsUser ={
     name: "Varsha",
+    "fullname": "Varsha Tomar",
+    [mySym] :"myKey1",    // to refer the symbol use the square breakets.
+    mySym:"mykey2", // it is a normal object value here not symbol.
     age: 21,
     location: "Gwalior",
     email: "varsha.tomar@gmail.com",
@@ -13,5 +18,23 @@ const jsUser ={
     lastLoginDays: ["monday","saturday"]
 }
 
+// console.log(jsUser["email"])
+// console.log(jsUser["fullname"])
+// console.log(jsUser.mySym)
+// console.log(jsUser[mySym])
+// console.log(typeof mySym)
+
+// to change the values
+
+jsUser.email ="barsha.tomar@gmail.com"
+Object.freeze(jsUser)
+jsUser.email ="radhika@crosoft.com"
+console.log(jsUser)
+
 console.log(jsUser["email"])
 
+jsUser.greeting = function(){
+    console.log("hello user")
+}
+
+console.log(jsUser.greeting);
