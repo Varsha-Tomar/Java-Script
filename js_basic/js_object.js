e656517f6215517d61fa1619e9dfb0f5e988544b
Varsha <varsha.v.tomar@gmail.com>
2025-06-17 04:56:49 +0000

    create object in java script

---
+++ js_basic/js_object.js
@@ -0,0 +1,17 @@
+// singleton 
+
+//  object literals 
+
+// Object.create
+
+const jsUser ={
+    name: "Varsha",
+    age: 21,
+    location: "Gwalior",
+    email: "varsha.tomar@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday","saturday"]
+}
+
+console.log(jsUser["email"])
+
