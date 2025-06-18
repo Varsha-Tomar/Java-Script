5d253477daaa5c96d7c99807f90ba172b263fabe
Varsha <varsha.v.tomar@gmail.com>
2025-06-18 11:16:23 +0000

    object access through object

--- js_basic/function.js
+++ js_basic/function.js
@@ -14,5 +14,29 @@ function calculateCartPrice(...num1) {
     return num1
 }
 
-console.log(calculateCartPrice(20,56,45))
+// console.log(calculateCartPrice(20,56,45))
+
+const user = {
+    username: "Hitesh",
+    price: 199
+}
+function handleObject(anyObject) {
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({      // here also used the object and it directly return the value(output)
+    username: "sam",
+    price: 799
+})
+
+const myNewArray = [400,300,200,100]
+function returnSecondValue(getArray) {
+    return getArray[3]
+}
+
+// console.log(returnSecondValue(myNewArray))
+
+console.log(returnSecondValue(4400,3300,8800,900));
 
