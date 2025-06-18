6fd5f9b7fbe94107b070a3046e978ec7f7effe55
Varsha <varsha.v.tomar@gmail.com>
2025-06-18 12:01:11 +0000

    scope done

--- js_basic/scope.js
+++ js_basic/scope.js
@@ -6,9 +6,47 @@ if(true) {
     let a =20
     const b = 40
     var c = 50
-    console.log("inner:",a)
+    // console.log("inner:",a)
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
-console.log(c)
+// console.log(c)
+
+// nested scope
+
+function one() {
+    const username = "Hitesh"
+
+    function two() {
+        const website = "youtube"
+        // console.log(username)
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true) {
+    const username = "Varsha"
+    if(username === "Varsha") {
+        const website = " Youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+// +++++++++++++++++++ interesting +++++++++++++++++++++++++
+
+addone(5)
+function addone(num) {
+    return num +1
+}
+
+addone(5)
+// addTwo(4)
+const addTwo = function(num) {
+    return num + 2
+}
+addTwo(5)
