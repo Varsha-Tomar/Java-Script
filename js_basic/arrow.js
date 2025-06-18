f606c6e46a60862de8517bb58cfd0638ab85363b
Varsha <varsha.v.tomar@gmail.com>
2025-06-18 12:15:45 +0000

    this function

---
+++ js_basic/arrow.js
@@ -0,0 +1,13 @@
+const user = {
+    username: "Varsha",
+    price: 999,
+
+    welcomeMessage: function () {
+        console.log(`${this.username}, welcome to website`)
+        console.log(this)
+    }
+
+}
+user.welcomeMessage()
+user.username = "Radhika"
+user.welcomeMessage()
