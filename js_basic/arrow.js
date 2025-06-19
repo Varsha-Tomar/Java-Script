d65e5013d18063c2d482869c6f8b5852e1b2672a
Varsha <varsha.v.tomar@gmail.com>
2025-06-19 06:22:55 +0000

    arrow function

--- js_basic/arrow.js
+++ js_basic/arrow.js
@@ -11,3 +11,28 @@ const user = {
 user.welcomeMessage()
 user.username = "Radhika"
 user.welcomeMessage()
+
+// function fruit() {
+//     let username ="varsha"
+//     console.log(this.username)
+// }
+
+// fruit()
+
+
+const fruit = () => {
+    let username ="varsha"
+    console.log(this)
+}
+
+fruit()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+const addTwo = (num1, num2) =>  num1 + num2
+// implicit pass if statement is one line no need to write return keyword
+
+console.log(addTwo(4,7))
+
