7bb57b476aa90feadc9c3b76015a7b7228676f4c
Varsha <varsha.v.tomar@gmail.com>
2025-06-20 05:20:23 +0000

    theory execution

---
+++ js_basic/execution_contest.js
@@ -0,0 +1,28 @@
+/* 
+Function Execution Context 
+
+1. Global execution Context
+2. function execution Context
+3. eval execution Context
+
+{} memoy (creation phase) phase
+
+execution phase 
+
+Global execution phase is (This)
+
+memory phase 
+
+val1 = undefined
+val2 = undefined
+addnum =defination
+result1 = undefined
+result2 = undefined
+
+execution phase
+
+val1 = 10
+val2 = 5
+
+
+*/
