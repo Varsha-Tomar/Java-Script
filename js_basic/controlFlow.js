ff8292e2663a20511f6caf5f891567d6fe7e6ba5
Varsha <varsha.v.tomar@gmail.com>
2025-06-20 12:04:20 +0000

    if else condition

--- js_basic/controlFlow.js
+++ js_basic/controlFlow.js
@@ -1,13 +1,23 @@
 
 // if
 
-const inUserLoggedIn = false
-
-if(inUserLoggedIn) {
-    
+const inUserLoggedIn = true
+const debitCard = true
+if(inUserLoggedIn && debitCard) {
+    console.log("Allow to shopping");
+}
+else {
+    console.log("Not allow to shopping");
 }
 const temperature = 41;
 if(temperature < 50) {
     console.log("less than 50")
 }
 console.log("Temparature greater than 50")
+
+if(temperature === 41) {
+    console.log("less than 50")
+}
+else {
+console.log("Temparature greater than 50")
+}
