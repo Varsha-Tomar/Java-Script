2ec0d4d3df5e463a0a16b10aaf1e23da00b9ab1d
Varsha <varsha.v.tomar@gmail.com>
2025-06-20 12:19:31 +0000

    true or false condition

---
+++ js_basic/truthly.js
@@ -0,0 +1,13 @@
+const userEmail = "v@rsha.ai"
+
+if(userEmail) {
+    console.log("Got user email");
+}
+else {
+    console.log("Don't have user email");
+}
+
+// false, 0, -0 BigInt 0n, "", null, undefined, NaN 
+
+// truthly values 
+// "0", 'false' " ", [], {}, function() {}
