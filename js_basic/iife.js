a3405df9ef8b027d1c23038228770044c041f7dd
Varsha <varsha.v.tomar@gmail.com>
2025-06-20 05:01:33 +0000

    semi collon

--- js_basic/iife.js
+++ js_basic/iife.js
@@ -2,5 +2,9 @@
 
 (function chai () {
     console.log('DB CONNECTED');
-})()
+})();  // it is neccessary to run next function otherwise next function produce errors in code.
+
+((name) => {
+    console.log(`Db Connected Two ${name}`);
+}) ('Varsha')
 
