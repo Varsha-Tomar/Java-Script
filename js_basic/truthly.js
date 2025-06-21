b87382d55942d5ac6a0ad7c27636fd6d4f2a8dcc
Varsha <varsha.v.tomar@gmail.com>
2025-06-21 02:08:38 +0000

    terniary and nullish operator

--- js_basic/truthly.js
+++ js_basic/truthly.js
@@ -11,3 +11,25 @@ else {
 
 // truthly values 
 // "0", 'false' " ", [], {}, function() {}
+
+// false == 0
+// true
+
+// Nullish coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5??10
+// val1 = null??10
+
+// val1 = undefined ?? 55
+
+val1 = null ?? 10 ?? 15
+
+console.log(val1);
+
+// Terniary operator
+
+// condition ? true: false
+
+const iceTeaPrice = 100
+iceTeaPrice <=80 ? console.log("Less than 80") : console.log("MOre than 80")
