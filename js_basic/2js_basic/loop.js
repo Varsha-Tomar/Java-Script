ce7cd67f47074d2366cdc76bfc0a17bbe613aab5
Varsha <varsha.v.tomar@gmail.com>
2025-06-21 02:34:57 +0000

    for loop explaination

---
+++ js_basic/2js_basic/loop.js
@@ -0,0 +1,28 @@
+// for loop
+
+let myarray = ["Varsha","Khushi","Anamika","Krishna"];
+
+for(let index =0; index < myarray.length; index++) {
+    const element = myarray[index];
+    
+    console.log(element);
+}
+
+// for(let index =0; index < 10; index++) {
+//     const element = index;
+//     if(element == 5) {
+//         console.log("5 is the best Number");
+//     }
+//     console.log(element);
+
+// }
+
+for(let i= 0; i< 10; i++) {
+    // console.log(`Outer loop ${i}`);
+    for(let j =0; j <10; j++) {
+        // console.log(`Inner loop ${j} and inner loop ${i}`);
+
+        // console.log(i + '*' + j +' = ' +i*j);
+    }
+}
+
